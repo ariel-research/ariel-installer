433e3fbb2c78a8b5f946e66fc86e2db6d86c3c16
David Baum <serjantik@gmail.com>
2023-07-23 19:28:35 +0200

    Updated the job

--- utils/apps.py
+++ utils/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class UtilsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'utils'
+
+    def ready(self):
+        from utils.jobs.scheduler import AITasksScheduler
+        tasks_scheduler = AITasksScheduler()
+        tasks_scheduler.check_new_commits()
+        tasks_scheduler.check_running_projects()
