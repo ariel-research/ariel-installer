8ba12361adbe9f1a97fb7a0c0aaaa8cf32078290
David Baum <serjantik@gmail.com>
2023-08-01 18:33:27 +0200

    Updated the script

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $HOME/arielinstaller/ && source .env/bin/activate
-gunicorn -b 0.0.0.0:8001 config.wsgi --daemon
-nohup python manage.py rqworker default low &
-nohup python manage.py rqscheduler &
+cd $HOME/arielinstaller/
+source .env/bin/activate && gunicorn -b 0.0.0.0:8001 config.wsgi --daemon
+source .env/bin/activate && nohup python manage.py rqworker default low &
+source .env/bin/activate && nohup python manage.py rqscheduler &
